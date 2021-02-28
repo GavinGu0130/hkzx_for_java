d0479eb1872f788e45e5ba7a3e1d749916e3a984
Gavin Gu <gavingu0130@hotmail.com>
2021-02-28 20:07:46 +0800

    thymeleaf demo

---
+++ src/main/java/hongkouZX/Controller/DemoController.java
@@ -0,0 +1,17 @@
+package hongkouZX.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("callhello")
+    public String helloABC(Model model){
+        model.addAttribute("gavin", "demo is success !");
+        model.addAttribute("gavin1", "hello1 is success !");
+        return "hello";
+    }
+}
---
+++ src/main/java/hongkouZX/config/DemoConfig.java
@@ -0,0 +1,17 @@
+package hongkouZX.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class DemoConfig implements WebMvcConfigurer {
+
+ /*   @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/callhello").setViewName("hello");
+    }*/
+}

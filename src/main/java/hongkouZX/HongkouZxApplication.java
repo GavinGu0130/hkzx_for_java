ef6b177dbcb8a731cd5365f992c03214200a9ef2
Gavin Gu <gavingu0130@hotmail.com>
2021-02-28 12:45:02 +0800

    spring initializr

---
+++ src/main/java/hongkouZX/HongkouZxApplication.java
@@ -0,0 +1,13 @@
+package hongkouZX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongkouZxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongkouZxApplication.class, args);
+	}
+
+}
